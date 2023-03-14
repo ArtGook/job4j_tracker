abc00ddeca0d7b5f986b5672ebee9edd5088481f
ArtGook <artemgabrusenok305@gmail.com>
2023-03-14 11:01:09 +0300

    1.5. Поля объекта [#166083 #66551]

--- src/main/java/ru/job4j/oop/DummyDic.java
+++ src/main/java/ru/job4j/oop/DummyDic.java
@@ -2,7 +2,7 @@ package ru.job4j.oop;
 
 public class DummyDic {
     public String engToRus(String eng) {
-        String rsl = "Неизвестное слово." + eng;
+        String rsl = "Неизвестное слово. " + eng;
         return rsl;
     }
 
