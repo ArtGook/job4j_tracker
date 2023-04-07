d6963eeca6d47385a80c71753df6a452960b89b7
ArtGook <artemgabrusenok305@gmail.com>
2023-04-07 10:50:46 +0300

    10. toString [#310018]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -4,10 +4,12 @@ import java.time.format.DateTimeFormatter;
 public class StartUI {
     public static void main(String[] args) {
         Item created = new Item();
+        Item test = new Item(8, "Arsen");
         System.out.println(created.getCreated());
 
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
         String currentDateTimeFormat = created.getCreated().format(formatter);
         System.out.println("Текущие дата и время после форматирования: " + currentDateTimeFormat);
+        System.out.println(test.toString());
     }
 }
